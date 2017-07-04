8711c3cedf65ae61988db8fbbbe5729e1d1f4520
pasha <pasha_mugako@mail.ru>
2017-07-05 02:17:10 +0300

    start to make table for timetable

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -5,6 +5,12 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class MainClass {
     public static void main(String[] args) throws IOException {
         String url = "http://www.minsktrans.by/mg/suburbt.php?find_runs=1&minsk=501113&other=501106";
@@ -30,5 +36,33 @@ public class MainClass {
         outFileHTML.close();
         in.close();
 
+
+        String html = "http://www.minsktrans.by/mg/suburbt.php?find_runs=1&minsk=501113&other=501106";
+        try {
+            Document doc = Jsoup.connect(html).get();
+            Elements tableElements = doc.select("table");
+
+            Elements tableHeaderEles = tableElements.select("thead tr th");
+            System.out.println("headers");
+            for (int i = 0; i < tableHeaderEles.size(); i++) {
+                System.out.println(tableHeaderEles.get(i).text());
+            }
+            System.out.println();
+
+            Elements tableRowElements = tableElements.select(":not(thead) tr");
+
+            for (int i = 0; i < tableRowElements.size(); i++) {
+                Element row = tableRowElements.get(i);
+                System.out.println("row");
+                Elements rowItems = row.select("td");
+                for (int j = 0; j < rowItems.size(); j++) {
+                    System.out.println(rowItems.get(j).text());
+                }
+                System.out.println();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
