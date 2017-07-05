8a5b0e325780e494efce51a924be5b8730ac0275
pasha <pasha_mugako@mail.ru>
2017-07-05 17:24:32 +0300

    start to make NumberBus

--- src/forGetSource/NumberBus.java
+++ src/forGetSource/NumberBus.java
@@ -1,4 +1,39 @@
 package forGetSource;
 
-public class NumberBus {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.TreeMap;
+
+public class NumberBus extends TreeMap<String, String> {
+    public NumberBus() {
+        super();
+    }
+    public void reWrite(){
+
+        String html = "http://www.minsktrans.by/mg/suburbt.php?find_runs=1&minsk=501113&other=501106";
+        try {
+            Document doc = Jsoup.connect(html).get();
+            Elements tableElements = doc.select("table[class=schedule_table]");
+
+            Elements tableRowElements = tableElements.select("tr");
+            for (int i = 0; i < tableRowElements.size(); i++) {
+                Element row = tableRowElements.get(i);
+                //System.out.println("row");
+                Elements rowItems = row.select("th");
+                if (rowItems.size() == 0)
+                    rowItems = row.select("td");
+                for (int j = 0; j < rowItems.size(); j++) {
+                    System.out.println(rowItems.get(j).text());
+                }
+                System.out.println();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -13,53 +13,33 @@ import org.jsoup.select.Elements;
 
 public class MainClass {
     public static void main(String[] args) throws IOException {
-        String url = "http://www.minsktrans.by/mg/suburbt.php?find_runs=1&minsk=501113&other=501106";
-        URL obj = new URL(url);
-        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
-        connection.setRequestMethod("GET");
-        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//        String url = "http://www.minsktrans.by/mg/suburb.php";
+//        URL obj = new URL(url);
+//        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+//        connection.setRequestMethod("GET");
+//        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//
+//        //make out.html file
+//        PrintWriter outFileHTML = new PrintWriter(new BufferedWriter(new FileWriter("out2.html")));
+//        String inputLine;
+//        while ((inputLine = in.readLine()) != null) {
+//            outFileHTML.println(inputLine);
+//        }
+//        outFileHTML.close();
+//        in.close();
 
-        //make out.html file
-        PrintWriter outFileHTML = new PrintWriter(new BufferedWriter(new FileWriter("out.html")));
-        outFileHTML.println("<html>\n" +
-                "\t<head>\n" +
-                "\t\t<title>Расписание</title>\n" +
-                "\t\t<meta http-equiv=\"Content-Type\" content=\"text/html\"; charset=\"utf-8\">\n" +
-                "\t</head>\n" +
-                "\t<body>");
-        String inputLine;
-        while ((inputLine = in.readLine()) != null) {
-            outFileHTML.println("\t\t" + inputLine);
-        }
-        outFileHTML.println("\t</body>\n" +
-                "</html>");
-        outFileHTML.close();
-        in.close();
-
-
-        String html = "http://www.minsktrans.by/mg/suburbt.php?find_runs=1&minsk=501113&other=501106";
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+        String html = "http://www.minsktrans.by/mg/suburb.php";
         try {
             Document doc = Jsoup.connect(html).get();
-            Elements tableElements = doc.select("table");
+            Elements tableElements = doc.select("select[id=minsk0]");
 
-            Elements tableHeaderEles = tableElements.select("thead tr th");
-            System.out.println("headers");
+            Elements tableHeaderEles = tableElements.select("option");
             for (int i = 0; i < tableHeaderEles.size(); i++) {
                 System.out.println(tableHeaderEles.get(i).text());
+                System.out.println(tableHeaderEles.get(i));
             }
             System.out.println();
-
-            Elements tableRowElements = tableElements.select(":not(thead) tr");
-
-            for (int i = 0; i < tableRowElements.size(); i++) {
-                Element row = tableRowElements.get(i);
-                System.out.println("row");
-                Elements rowItems = row.select("td");
-                for (int j = 0; j < rowItems.size(); j++) {
-                    System.out.println(rowItems.get(j).text());
-                }
-                System.out.println();
-            }
 
         } catch (IOException e) {
             e.printStackTrace();
